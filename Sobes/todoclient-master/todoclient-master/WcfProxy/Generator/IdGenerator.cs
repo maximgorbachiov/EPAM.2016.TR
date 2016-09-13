f3b9cbc133cb14c8fdc5416b420b1eb2bb26c61f
Sergu <sergushov.1995@mail.ru>
2016-09-13 18:16:47 +0300

    added id generator

---
+++ Sobes/todoclient-master/todoclient-master/WcfProxy/Generator/IdGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebInterfaces.Interfaces;
+
+namespace WcfProxy.Generator
+{
+    public class IdGenerator : IIdGenerator
+    {
+        private int position = 0;
+
+        public IdGenerator(int position)
+        {
+            this.position = position;
+        }
+        public IdGenerator() { }
+
+
+        public int GetId()
+        {
+            return position++;
+        }
+
+        public void SetCurentPosition(int position)
+        {
+            this.position = position;
+        }
+
+        public int GetCurrentPosition()
+        {
+            return this.position;
+        }
+    }
+}
---
+++ Sobes/todoclient-master/todoclient-master/WebInterfaces/Interfaces/IIdGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebInterfaces.Interfaces
+{
+    public interface IIdGenerator
+    {
+        int GetId();
+        void SetCurentPosition(int position);
+        int GetCurrentPosition();
+    }
+}
